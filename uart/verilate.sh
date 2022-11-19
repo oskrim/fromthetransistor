4743dd7e0dc2fd30f4f4ba30e5ad835a4bc9f0d1
Oskari Mantere <osku.mantere@gmail.com>
2022-11-19 08:36:56 +0200

    update verilate.sh

--- uart/verilate.sh
+++ uart/verilate.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-verilator --cc --exe --build -j 0 -Wall main.cpp v/uart.v && ./obj_dir/Vuart
+verilator --cc --exe --build -j 0 -Wall main.cpp v/uart.v v/*_uart.v && ./obj_dir/Vuart
